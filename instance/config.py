6fc9ce809e6293c876a8501f9fdd71960f652c2e
root <root@localhost.localdomain>
2018-11-25 14:42:48 -0500

    add app configuration settings [starts #162184218]
    The app configuration will enable the app to run with the debug mode true both in development and testing while in production debug mode will be false
    
    updated .gitignore with .vscode/ so that they are not pushed

---
+++ instance/config.py
@@ -0,0 +1,27 @@
+"""This module handles app configuration settings """
+
+import os
+class Config:
+    """This class holds a common config for all environments"""
+    SECRET_KEY = os.environ.get('SECRET_KEY') or '@#57#%@'
+
+class DevelopmentConfig(Config):
+    """Configurations for the development environment"""
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    """configuration for the testing environment"""
+    DEBUG = True
+
+class ProductionConfig(config):
+    """configuration for the production environment"""
+    DEBUG = False
+
+
+config = {
+       'development': DevelopmentConfig,
+       'testing'    : TestingConfig,
+       'production': ProductionConfig,
+       'default': DevelopmentConfig
+}
