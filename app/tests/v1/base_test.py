290500a48240ace667b8d046f6a114a32754651c
root <root@localhost.localdomain>
2018-11-26 14:29:31 -0500

    [finishes #162180789] add BaseTestCase
    The Base test case contains the test client and different variety of data to be tested
    
    add test __pycache__ folder to gitignore so that it is not add to git

---
+++ app/tests/v1/base_test.py
@@ -0,0 +1,95 @@
+from unittest import TestCase
+
+#local imports
+from instance import create_app
+from api.v1.models import redflag_db
+
+class BaseTestCase(TestCase):
+    def setUp(self):
+        self.app = create_app('testing')
+        self.db = redflag_db
+        self.client = self.app.test_client()
+
+        self.redflag_data = {
+             "createdBy": "Larry karani",
+             "type" : "red-flag",
+             "location": "123456, 23434",
+             "Images": [],
+             "videos":[],
+             "comment":"String"
+         }      
+        self.redflag_data_with_empty_created={
+             "createdBy": " ",
+             "type" : "red-flag",
+             "location": "123456, 23434",
+             "Images": [],
+             "videos":[],
+             "comment":"String"
+        }
+        self.redflag_data_with_empty_type ={
+             "createdBy": "Larry karani",
+             "type" : " ",
+             "location": "123456, 23434",
+             "Images": [],
+             "videos":[],
+             "comment":"String"
+        }
+
+        self.redflag_data_empty_location = {
+             "createdBy": "Larry karani",
+             "type" : "red-flag",
+             "location": " ",
+             "Images": [],
+             "videos":[],
+             "comment":"String"
+         }
+
+        self.redflag_data_empty_location = {
+             "createdBy": "Larry karani",
+             "type" : "red-flag",
+             "location": " ",
+             "Images": [],
+             "videos":[],
+             "comment":"String"
+         }
+
+
+
+        self.redflag_data_invalid_location = {
+             "createdBy": "Larry karani",
+             "type" : "red-flag",
+             "location": "long, lat",
+             "Images": [],
+             "videos":[],
+             "comment":"String"
+         }
+
+      
+       
+    
+
+        self.update_redflag_data = {
+             "createdBy": "james Irungu",
+             "type" : "intervention",
+             "location": " ",
+             "Images": [],
+             "videos":[],
+             "comment":"String"
+         }
+        
+
+        self.redflag_data_invalid_type = {
+             "createdBy": "Larry karani",
+             "type" : "bundesliga",
+             "location": "long, lat",
+             "Images": [],
+             "videos":[],
+             "comment":"String"
+         }
+
+
+    def tearDown(self):
+        """clean Db"""
+        self.db.clear()
+        self.client=None
+
