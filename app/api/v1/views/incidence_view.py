647ddec226378c85ead9cae4aef1794ad77954bc
root <root@localhost.localdomain>
2018-11-27 17:43:16 -0500

    add method to allow users get an incidence [finishes #162183212]
    the get_an_incidence method rturs the data and the status code

--- app/api/v1/views/incidence_view.py
+++ app/api/v1/views/incidence_view.py
@@ -5,7 +5,7 @@ import werkzeug
 #local import 
 from app.api.v1.models.incidence_model import Incidence, db
 
-v1_incedence = Namespace('Incedences')
+v1_incedence = Namespace('red-flags')
 
 incidence_data = v1_incedence.model('Incidence',{
                        'createdBy' :fields.String(description='name of the user creating the red-flag'), 
@@ -14,12 +14,11 @@ incidence_data = v1_incedence.model('Incidence',{
                        'comment': fields.String(description='name of the user creating the red-flag')
 })
 
-class Incedence(Resource):
+class Incidences(Resource):
     '''Shows a list of all incedences and allow users to create a new incedence'''
 
-    @v1_incedence.doc('list_incedence')
     def get(self):
-        '''List all tasks'''
+        '''List all incidence'''
 
         return db, 200
 
@@ -29,14 +28,49 @@ class Incedence(Resource):
         '''Create a new incidence'''
 
         data = v1_incedence.payload
-        new_instance = Incidence(data['createdBy'],data['type'], data['location'],data['comment'])
-        output = new_instance.create_incidence()
+        new_instance = Incidence()
+        output = new_instance.create_incidence(data['createdBy'],data['type'], data['location'],data['comment'])
         return {
-            "message": "incedence created",
-            "data": output
-        }
+                'status' : 201, 
+                'data' : [{
+                           'id' : output['id'],   
+                           'message' :  'Created red-flag record'
+                           }]
+               }
 
-v1_incedence.add_resource(Incedence, '/incidence', strict_slashes=False)
+class AnIncidence(Resource):
+    '''gets a single incidence, updates an incidence, change the status of an incidence'''
+  
+    def get(self, red_id):
+        '''Returns details of a specific incidence'''
+        new_instance = Incidence()
+        response=new_instance.get_an_incidence(red_id)
+        if len(response)==0:
+            return {'message': 'incidence does not exist'}, 400
+
+        return {
+                  'status': 200, 
+                  'data' : response
+               }
+
+    def delete(self, red_id):
+        '''deletes a specific incidence'''
+        new_instance = Incidence()
+        response = new_instance.delete(red_id)
+
+        if len(response)==0:
+            return {'message': 'incidence does not exist'}, 400
+
+        return {
+             'status':200,
+             'data' : response
+              }
+       
+
+
+
+v1_incedence.add_resource(Incidences, '/' , strict_slashes=False)
+v1_incedence.add_resource(AnIncidence, '/<int:red_id>', strict_slashes=False)
 
         
 
