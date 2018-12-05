f878463d44cb85d539d51632803a4b7a234f676a
root <root@localhost.localdomain>
2018-12-05 01:44:41 -0500

    [finishes #162437744]validate comment does not contain invalid charactors

--- app/api/v1/validators/validate_incidence.py
+++ app/api/v1/validators/validate_incidence.py
@@ -10,9 +10,17 @@ def validate_length(input):
 
 
 class IncidenceSchema(Schema):
+
     '''Validates incidence data'''
 
     createdBy =    fields.String(required=True, validate=validate_length)
     location  = fields.String(required=True, validate=validate_length)
     comment   = fields.String(required=True,  validate=validate_length)
     incidence_type =fields.String(required=True, validate=validate_length)
+
+    @validates('comment')
+    def validate_length(self, comment):
+        if comment.strip() == '':
+            raise ValidationError('fields cannot be blank')
+        elif not re.match(r"^(?=.*[a-z])[a-zA-Z0-9_.-]{3,25}$", comment):
+            raise ValidationError("{} is not a valid input".format(comment))
