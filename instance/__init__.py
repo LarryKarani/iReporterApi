bb67c0b766efe97e20e7015fa3d191ae7a4755ee
root <root@localhost.localdomain>
2018-12-07 23:54:41 -0500

    FIX: made custom error user friendly

--- instance/__init__.py
+++ instance/__init__.py
@@ -24,7 +24,7 @@ def create_app(config_name):
     # this function hundles 404 errors
     @app.errorhandler(404)
     def page_not_found(e):
-        return jsonify({"error": "page not found"}), 404
+        return jsonify({"error": "oops! page not found"}), 404
 
     return app
     
