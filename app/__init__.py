f998b71a40aa67dfbb064d303bc05a386746450b
root <root@localhost.localdomain>
2018-12-11 11:53:43 -0500

    change default admin to False and fix travis build

---
+++ app/__init__.py
@@ -0,0 +1,30 @@
+from flask import Flask,  jsonify
+from app.api.v1 import v1_blueprint
+from app.api.v2 import v2_blueprint
+from .api.v2 import v2_api
+from app.api.v2.models.db import Db
+from flask_jwt_extended import JWTManager
+db_object=Db()
+
+from instance.config import config
+#this function returns the instance of the app
+def create_app(config_name):
+    """app factory creates the instance of the app"""
+    app = Flask(__name__)
+    db_object.create_tables()
+    jwt = JWTManager(app)
+    app.config['JWT_SECRET_KEY'] = '%****#@#'
+    app.config.from_object(config[config_name])
+    
+    app.register_blueprint(v2_blueprint, url_prefix="/api/v2")
+    app.register_blueprint(v1_blueprint, url_prefix="/api/v1")
+    api = v2_api
+
+    jwt._set_error_handler_callbacks(api)
+    # this function hundles 404 errors
+    @app.errorhandler(404)
+    def page_not_found(e):
+        return jsonify({"error": "oops! page not found"}), 404
+
+    return app
+    
--- run.py
+++ run.py
@@ -1,9 +1,5 @@
-from instance import create_app
-from app.api.v2.models.db import Db
+from app import create_app
 
-db_object=Db()
 app = create_app('default')
-db_object.create_tables()
 if __name__ == '__main__':
     app.run()
-    
