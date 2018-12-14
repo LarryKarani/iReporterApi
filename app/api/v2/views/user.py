795fd6532793d9bdb41ccb57b6d260161e37ef1c
root <root@localhost.localdomain>
2018-12-14 08:06:51 -0500

    [finishes #162648044]remove password from data after signup

--- app/api/v2/views/user.py
+++ app/api/v2/views/user.py
@@ -4,24 +4,26 @@ from flask_jwt_extended import create_access_token
 from werkzeug.security import check_password_hash
 from app.api.v2.views import blacklist
 from flask_jwt_extended import jwt_required, get_raw_jwt
-#local import
+# local import
 from app.api.v2.models.users import User
 from app.api.v2.validators.validate_user import UserSchema, LoginSchema
 
 
 v2_user = Namespace('auth')
-registration_args_model=v2_user.model(
+registration_args_model = v2_user.model(
     'Regestration', {
-    "firstname": fields.String(description='username'),
-    "lastname": fields.String(description='username'),
-    "othername": fields.String(description='username'),
-    "password": fields.String(description='password'),
-    "email": fields.String(description='email'),
-    "phoneNumber": fields.String(description='phoneNumber'),
-    "username": fields.String(description='username')})
+        "firstname": fields.String(description='username'),
+        "lastname": fields.String(description='username'),
+        "othername": fields.String(description='username'),
+        "password": fields.String(description='password'),
+        "email": fields.String(description='email'),
+        "phoneNumber": fields.String(description='phoneNumber'),
+        "username": fields.String(description='username')})
 
-user_login = v2_user.model('Login', {'username': fields.String('email@example.com'),
-                                     'password': fields.String('test_pass')})
+user_login = v2_user.model('Login',
+                           {'username': fields.String('email@example.com'),
+                            'password': fields.String('test_pass')})
+
 
 class Register(Resource):
     @v2_user.expect(registration_args_model)
@@ -32,22 +34,22 @@ class Register(Resource):
         schema_data = schema.load(data)
 
         errors = schema_data.errors
-        error_types = ['firstname', 'lastname', 'othername', 'password', 'email','phoneNumber','username']
+        error_types = ['firstname', 'lastname', 'othername',
+                       'password', 'email', 'phoneNumber', 'username']
 
         for e in error_types:
             if e in errors.keys():
                 return {'message': errors[e][0]}, 400
-        
+
         new_user = User(
-                      data['firstname'],
-                      data['lastname'],
-                      data['othername'],
-                      data['email'],
-                      data['phoneNumber'],
-                      data['username'],
-                      data['password']
-                     )
-       #check if user already exists
+            data['firstname'],
+            data['lastname'],
+            data['othername'],
+            data['email'],
+            data['phoneNumber'],
+            data['username'],
+            data['password']
+        )
         user = new_user.check_username(data['username'])
         if user:
             return {'message': 'Username already exists'}, 400
@@ -57,12 +59,14 @@ class Register(Resource):
             return {'message': 'Email already exists'}, 400
 
         new_user.register_user()
+        del data['password']
         return {'message': 'User successfuly  added',
-                 'data': data
+                'data': data
                 }, 201
 
+
 class Login(Resource, User):
-    @v2_user.expect(user_login )
+    @v2_user.expect(user_login)
     def post(self):
         'logs in a user'
         data = v2_user.payload
@@ -75,11 +79,11 @@ class Login(Resource, User):
             if e in errors.keys():
                 return {'message': errors[e][0]}, 400
 
-        current_user= User.check_username(data['username'])
-        
+        current_user = User.check_username(data['username'])
+
         if not current_user:
             return {'message': 'Username does not exist'}, 400
-               
+
         if not check_password_hash(current_user[9], data['password'].strip()):
             return {'message': f'Invalid username or password'}, 400
 
